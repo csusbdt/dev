57d345a88aab136e19112cab087eab5042355249
Dave Turner <csusbdt@gmail.com>
2022-03-05 18:23:30 -0800

    X

--- docs/index.js
+++ docs/index.js
@@ -10,25 +10,39 @@ const blop = sfx("../sfx/blop_0.264.mp3", .5);
 // test
 //const bg = image(i_palace_of_electricity).start();
 //set_design(bg.i.width, bg.i.height);
-const palace_of_electricity = background(i_palace_of_electricity);
 
-const w = palace_of_electricity.i.width;
-const h = palace_of_electricity.i.height;
-const r_left   = rect(-w/3, 0, w/3, h).start();
-const r_middle = rect(   0, 0, w/3, h).start();
-const r_right  = rect( w/3, 0, w/3, h).start();
-// test
-//rect(   0, 0, w/3, h).vert(0, 0, w, h/3).start();
+const palace        = background(i_palace       );
+const san_francisco = background(i_san_francisco);
+const india         = background(i_india        );
 
-const t_left   = touch(r_left  ).starts(blop);
-const t_middle = touch(r_middle).starts(blop);
-const t_right  = touch(r_right ).starts(blop);
+function f(bg, left, right) {
+    const w        = bg.i.width;
+    const h        = bg.i.height;
+    const r_left   = rect(-w/3, 0, w/3, h);
+    const r_middle = rect(   0, 0, w/3, h);
+    const r_right  = rect( w/3, 0, w/3, h);
+    const t_left   = touch(r_left  ).stops(bg).starts(left);
+    const t_middle = touch(r_middle).starts(blop);
+    const t_right  = touch(r_right ).stops(bg).starts(right);
+    const touches  = [t_left, t_middle, t_right];
+    return touches;
+}
 
-const touches = [t_left, t_middle, t_right];
+const t_palace        = f(palace, india, san_francisco);
+const t_san_francisco = f(san_francisco, palace, india);
+const t_india         = f(india, san_francisco, palace);
 
-t_left.starts(touches);
-t_middle.starts(touches);
-t_right.starts(touches);
+t_palace       [1].starts(t_palace       );
+t_san_francisco[1].starts(t_san_francisco);
+t_india        [1].starts(t_india        );
 
-palace_of_electricity.start();
-start_start_sets(touches);
+t_palace       [2].starts(t_san_francisco);
+t_san_francisco[2].starts(t_india        );
+t_india        [2].starts(t_palace       );
+
+t_palace       [0].starts(t_india        );
+t_india        [0].starts(t_san_francisco);
+t_san_francisco[0].starts(t_palace       );
+
+palace.start();
+start_start_sets(t_palace);
